a2efa7d751f1ae47db95fe6086cdccd20eee74d9
Richard Marcus <richard.marcus@fau.de>
2018-10-21 01:05:11 +0200

    working web version

--- TGView3D/Assets/Scripts/FlyCamera.cs
+++ TGView3D/Assets/Scripts/FlyCamera.cs
@@ -96,7 +96,7 @@ public class FlyCamera : MonoBehaviour
     }
     private void Start()
     {
-        GameObject.Find("VR").SetActive(false);
+       // GameObject.Find("VR").SetActive(false);
          XRSettings.enabled = false;
     }
 }
